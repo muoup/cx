c23f520f15b54550c29fe685f08a5b27658e763c
muoup <masterdust12@gmail.com>
2025-05-25 22:26:07 -0400

    Not quite sure how I want to approach this, will decide later.

---
+++ compiler/cx-compiler-annotations/src/lib.rs
@@ -0,0 +1,7 @@
+use cx_data_annotations::AnnotationMap;
+
+pub fn parse_annotations(input: &str) -> Option<AnnotationMap> {
+    let mut iter = input.chars();
+    
+    None
+}
--- compiler/cx-data-annotations/src/lib.rs
+++ compiler/cx-data-annotations/src/lib.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+
 pub struct TypeAnnotation {
     pub name: String,
-    
+    pub assertion: String
 }
+
+pub type AnnotationMap = HashMap<String, TypeAnnotation>;
