23a2f156f09dce1265550b90747ea65ca26ada38
Zachary Verlardi <zacharyverlardi@gmail.com>
2026-08-18 13:36:53 -0400

    refactor: remove unused THIR generation request

--- compiler/cx-thir/src/lib.rs
+++ compiler/cx-thir/src/lib.rs
@@ -1,10 +1,9 @@
-use cx_hir::ast::function::HIRFunctionKind;
 pub use cx_util::namespace::EnvironmentNamespace;
 
 use crate::{
     registry::THIRDecomposedRegistry,
     thir::{
-        data::{MIRTemplateInput, THIRFunction},
+        data::THIRFunction,
         global::MIRGlobalVariable,
     },
 };
@@ -17,12 +16,6 @@ pub mod thir;
 pub mod type_context;
 
 mod format;
-
-pub struct MIRGenerationRequest {
-    pub module_origin: Option<String>,
-    pub kind: HIRFunctionKind,
-    pub input: MIRTemplateInput,
-}
 
 #[derive(Debug, Clone)]
 pub struct THIRUnit {
